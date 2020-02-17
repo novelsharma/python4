b003604ebb8be3da18fccc7ce4562c756690e128
novelsharma <novelsharma018@gmail.com>
2020-02-18 01:57:52 +0600

    there are some code file

---
+++ basic.py
@@ -0,0 +1,30 @@
+"""print('Hello Bangladesh')
+
+print('Float = '+ str(10.5)+'Integer = ', 50)"""
+"""a = 12
+print(a)
+print (a *4)
+print(a + a*4)
+print(a * a * 4)
+print(a-6)"""
+
+"""days=['monday','friday','tuesday','sunday']
+print(days)
+
+house_cost = 1000
+iron_cost = 100
+food_cost = 200
+
+
+total = house_cost+ iron_cost+ food_cost
+
+print("Total Cost: {0}".format(total))"""
+
+novel_sharma =1000
+raju_sharma =1000
+saju_sharma =1000
+pabel_sharma =1000
+cost =novel_sharma + raju_sharma + saju_sharma + pabel_sharma
+print(f"Full cost: {cost}")
+
+print('Hello Bangladesh')
